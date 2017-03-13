de6ac30102c118763ebc4caff7ad80ff0f504675
Daniel Souza <danielmarcos2@yahoo.com.br>
2017-03-13 17:36:04 -0300

    mudança

--- config/parameters.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    tokenKey: 'aPiSlInGanaliTics',
-    adminPassword: 'yourPass',
-    gameKey: 'SlInGshOTtCc',
-    port: process.env.PORT || '8080',
-    urlDb: process.env.MONGODB_URI || 'mongodb://localhost:27017/slingshot',
-    testUrlDb: 'mongodb://localhost:27017/slingshot_test'
-}
